8b25c81d47c9094eb6ada486edd86ae58e985437
Hilmy <the.real.a.pacifist@gmail.com>
2025-11-08 18:34:20 +0700

    init global files

---
+++ script/player/player_buff_manager.gd
@@ -0,0 +1,28 @@
+extends Node
+
+class_name player_buff_manager
+
+
+var list_of_buffs = [] //list of didferent data objects
+
+enum buff_types {
+	Hood,
+	Rabbit,
+	Wizard,
+	Pig,
+	Cinderalla
+}
+
+
+
+func calculate_ all(list_off).
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
