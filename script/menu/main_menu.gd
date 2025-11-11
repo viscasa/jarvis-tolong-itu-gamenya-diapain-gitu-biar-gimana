203591db6999b1e222a0230b693ad46a8c5a503b
viscasa <attaralwie@gmail.com>
2025-11-12 01:27:08 +0700

    feat: audio manager

--- script/menu/main_menu.gd
+++ script/menu/main_menu.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _on_start_button_pressed() -> void:
 	pass
 	#TODO ganti ke path scene tutorial/prolog
-	#get_tree().change_scene_to_file("")
+	get_tree().change_scene_to_file("res://scene/level/world.tscn")
 
 
 
